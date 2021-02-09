a4a672c5b275afc6e9e2d48cce4c9acdab35d978
barlax91 <77514352+barlax91@users.noreply.github.com>
2021-02-09 14:58:32 +0100

    Update helloo.java

--- src/testgit/helloo.java
+++ src/testgit/helloo.java
@@ -1,10 +1,19 @@
 package testgit;
-
+import java.util.Scanner;
 public class helloo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String Noms = " ";
+		if(Noms!= " ") {
 	System.out.println("hello world");
+	System.out.println(Noms);
+		}else {
+			System.out.println(" veuillez saisir les noms des participants");
+			String str = sc.nextLine();
+			System.out.println(" voici le nom des participants :"+str);
+		}
 	}
 
 }
